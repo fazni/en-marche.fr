dd000523e1c9de458619fc39b90c02b7e7347c9a
Jeremie Samson <jeremie.samson76@gmail.com>
2019-03-26 15:26:09 +0100

    [Assessor] Add migration

--- src/Controller/EnMarche/AssessorManagerController.php
+++ src/Controller/EnMarche/AssessorManagerController.php
@@ -2,25 +2,13 @@
 
 namespace AppBundle\Controller\EnMarche;
 
-use AppBundle\Entity\ElectionRound;
-use AppBundle\Entity\ProcurationProxy;
-use AppBundle\Entity\ProcurationRequest;
-use AppBundle\Exception\ProcurationException;
-use AppBundle\Procuration\Filter\ProcurationProxyProposalFilters;
-use AppBundle\Procuration\Filter\ProcurationRequestFilters;
 use AppBundle\Procuration\ProcurationManager;
-use AppBundle\Repository\ProcurationRequestRepository;
-use Doctrine\DBAL\Driver\DriverException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 /**
  * @Route("/espace-responsable-assesseur")
@@ -35,7 +23,7 @@ class AssessorManagerController extends Controller
     public function requestsAction(Request $request, ProcurationManager $manager): Response
     {
         return $this->render('assessor_manager/requests.html.twig', [
-            'requests' => []
+            'requests' => [],
         ]);
     }
 }
